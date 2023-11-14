a5d77ac7d2c08b030f32abdc058c1defce1d3d3d
TREE KIM <sky_9233@naver.com>
2023-11-14 13:18:38 +0900

    이렇게 쉬운걸 고민하다니

--- 1026_보물.cpp
+++ 1026_보물.cpp
@@ -6,63 +6,24 @@ int main() {
 	int nArray;
 	cin >> nArray;
 
-	vector<pair<int, int>> vArrayA, vArrayB;
+	vector<int> vArrayA(nArray), vArrayB(nArray);
 
 	for (int i = 0; i < nArray; i++) {
-		int nTemp;
-		cin >> nTemp;
-		vArrayA.push_back(make_pair(i, nTemp));
+		cin >> vArrayA[i];
 	}
 
 	for (int i = 0; i < nArray; i++) {
-		int nTemp;
-		cin >> nTemp;
-		vArrayB.push_back(make_pair(i, nTemp));
+		cin >> vArrayB[i];
 	}
 
-	sort(vArrayA.begin(), vArrayA.end(),
-		[&](const pair<int, int>& n1, const pair<int, int>& n2) {
-		int nBeforeSum = 0, nAfterSum = 0;
-		for (int i = 0; i < nArray; i++) {
-			if (i == n1.first) {
-				nAfterSum += (n2.second * vArrayB[i].second);
-			} else if (i == n2.first) {
-				nAfterSum += (n1.second * vArrayB[i].second);
-			} else {
-				nAfterSum += (vArrayA[i].second * vArrayB[i].second);
-			}
-			nBeforeSum += (vArrayA[i].second * vArrayB[i].second);
-		}
-		if (nBeforeSum > nAfterSum) {
-			return true;
-		}
-		else {
-			return false;
-		}
-	});
-
-	for (auto a : vArrayA) {
-		cout << a.second << ' ';
-	}
-	cout << endl;
-
+	sort(vArrayA.begin(), vArrayA.end());
+	sort(vArrayB.begin(), vArrayB.end(), greater<>());
+	
 	int nSum = 0;
 	for (int i = 0; i < nArray; i++) {
-		nSum += (vArrayA[i].second * vArrayB[i].second);
+		nSum += vArrayA[i] * vArrayB[i];
 	}
 	cout << nSum;
 
 	return 0;
 }
-
-/*
-
-
-예제 입력 1
-5
-1 1 1 6 0
-2 7 8 3 1
-예제 출력 1
-18
-
-*/
