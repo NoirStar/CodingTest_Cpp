6a9d4c446fab84b73493dabe2ae71a8570497f4e
TREE KIM <sky_9233@naver.com>
2023-11-22 08:09:10 +0900

    다른방법으로 품 이게 나은듯 꼼수?

--- 10816_숫자카드2.cpp
+++ 10816_숫자카드2.cpp
@@ -3,9 +3,6 @@
 using namespace std;
 
 int main() {
-	cin.tie(NULL);
-	ios_base::sync_with_stdio(false);
-
 	int nCard, nInt;
 	cin >> nCard;
 	map<int,int> mCard;
@@ -13,12 +10,11 @@ int main() {
 	for (int i = 0; i < nCard; i++) {
 		int nTemp;
 		cin >> nTemp;
-		auto it = mCard.find(nTemp);
-		if (it == mCard.end()) {
-			mCard.insert(make_pair(nTemp, 1));
+		if (mCard[nTemp] == 0) {
+			mCard[nTemp] = 1;
 		}
 		else {
-			(*it).second++;
+			mCard[nTemp] += 1;
 		}
 	}
 
@@ -28,20 +24,9 @@ int main() {
 	for (int i = 0; i < nInt; i++) {
 		cin >> vInt[i];
 	}
-
-	vector<int> vRet;
+	
 	for (const auto& i : vInt) {
-		auto it = mCard.find(i);
-		if (it == mCard.end()) {
-			vRet.push_back(0);
-		}
-		else {
-			vRet.push_back((*it).second);
-		}
-	}
-
-	for (const auto& i : vRet) {
-		cout << i << " ";
+		cout << mCard[i] << ' ';
 	}
 
 	return 0;
